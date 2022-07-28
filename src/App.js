37df7fc091f9c6729b6e95207e715a7790de1c89
linfeng Chen <ku2003ku@gmail.com>
2022-07-28 00:17:28 -0400

    v1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+// import Event from "./components/Event"
+import Product from "./components/Product"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Product
+          title="Apple AirPods"
+          description="Lorem ipsum dolor sit amet, graecis intellegat pertinacia cum et"
+          image="https://cdn0.iconfinder.com/data/icons/household-items-2/100/airpod2-512.png"
+        />
       </header>
+
+      {/* <Event /> */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Event.jsx
@@ -0,0 +1,15 @@
+function Event() {
+  //empty tags = fragments
+
+  return (
+    <>
+      <h2>Concert</h2>
+      <h3>222</h3>
+      <p>7:00 pm - 10:00 pm</p>
+    </>
+  )
+}
+
+export default Event
+
+// {same as div aka css}
