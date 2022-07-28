25021b6538888d22593629e8ed569a1469f80139
linfeng Chen <ku2003ku@gmail.com>
2022-07-27 23:37:59 -0400

    v1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+// src/App.js
+import Settings from "./components/Settings"
+import "./App.css"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Quiz App</h1>
+      <Settings />
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
